9e7ded41c0315c41147db61c2940fcd2c00031c7
theanh-tran <anh.tran@cubicstack.net>
2025-11-10 21:53:35 +0700

    new fie

--- docker/start.sh
+++ docker/start.sh
@@ -20,14 +20,14 @@ fi
 
 # Update Nginx config với PORT từ env (Render inject $PORT)
 PORT=${PORT:-8080}
-sed -i "s/listen 8080;/listen $PORT;/g" /etc/nginx/conf.d/default.conf  # Đảm bảo đúng đường dẫn
+sed -i "s/listen 8080;/listen $PORT;/g" /etc/nginx/conf.d/default.conf
 
 # Đảm bảo thư mục run cho PHP-FPM
 mkdir -p /var/run/php
 chown www-data:www-data /var/run/php
 
 # Start PHP-FPM foreground (không daemonize)
-php-fpm8.2 -F  # Sử dụng -F thay vì -D
+php-fpm -F
 
 # Start Nginx foreground
 nginx -g 'daemon off;'

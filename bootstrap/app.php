dcb9be346e54ac9157e3b758c520acc0c8bf464c
theanh-tran <anh.tran@cubicstack.net>
2025-11-11 22:22:50 +0700

    new fie

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin.auth' => \App\Http\Middleware\AdminAuth::class,
         ]);
+        // Trust all proxies (Render uses load balancer)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
